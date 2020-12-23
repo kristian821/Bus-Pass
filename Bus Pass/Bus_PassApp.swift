b682e966ee5030aac03dddba650251e729a26609
kristian821 <56667566+kristian821@users.noreply.github.com>
2020-12-23 10:13:40 -0800

    Initial Commit

---
+++ Bus Pass/Bus_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bus_PassApp.swift
+//  Bus Pass
+//
+//  Created by Kristian Mitchell on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Bus_PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
